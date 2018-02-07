258a1873a7f77ac362de52aed276e195cef9d4b9
Demetrius Albuquerque <demetrius.albuquerque@yahoo.com.br>
2018-02-06 21:48:37 -0400

    added inspections controller and model

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :posts
   resources :costumers
   resources :places
+  resources :inspections
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180206214643) do
+ActiveRecord::Schema.define(version: 20180207014702) do
 
   create_table "costumers", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 20180206214643) do
     t.string "address"
     t.string "contact"
     t.string "fone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "inspections", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
