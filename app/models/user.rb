bd1fc8f564e5fde801fe4886023d65659d169cb6
Demetrius Albuquerque <demetrius.albuquerque@yahoo.com.br>
2018-02-08 18:38:45 -0400

    added validation to user fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :name, presence: true
+  validates :email, uniqueness: true
+  mount_base64_uploader :picture, PictureUploader
 end
