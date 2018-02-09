5a9d39852cb7f62f9005e3b00623e7ad54335ea9
Demetrius Albuquerque <demetrius.albuquerque@yahoo.com.br>
2018-02-09 03:53:52 -0400

    create occurrence with deadline problema and severity id as optional for first moment

--- app/models/occurrence.rb
+++ app/models/occurrence.rb
@@ -1,11 +1,11 @@
 class Occurrence < ApplicationRecord
   has_one :solution
-  belongs_to :deadline
-  belongs_to :problem
-  belongs_to :severity
+  belongs_to :deadline, class_name: 'Deadline', optional: true
+  belongs_to :problem, class_name: 'Problem', optional: true
+  belongs_to :severity, class_name: 'severity', optional: true
   belongs_to :inspection
-  
   belongs_to :user
+
   mount_base64_uploader :picture, PictureUploader
   
   def has_solution?
