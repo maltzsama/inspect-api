589031dd7a66c7eb307155dd4e1cc13172ddea94
Demetrius Albuquerque <demetrius.albuquerque@yahoo.com.br>
2018-02-15 01:41:08 -0400

    initializated unit test of system

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180215010751) do
+ActiveRecord::Schema.define(version: 20180209082003) do
 
   create_table "costumers", force: :cascade do |t|
     t.string "name"
@@ -29,21 +29,6 @@ ActiveRecord::Schema.define(version: 20180215010751) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "delayed_jobs", force: :cascade do |t|
-    t.integer "priority", default: 0, null: false
-    t.integer "attempts", default: 0, null: false
-    t.text "handler", null: false
-    t.text "last_error"
-    t.datetime "run_at"
-    t.datetime "locked_at"
-    t.datetime "failed_at"
-    t.string "locked_by"
-    t.string "queue"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.index ["priority", "run_at"], name: "delayed_jobs_priority"
   end
 
   create_table "inspections", force: :cascade do |t|
