c118a698433276adfe5a6920e1ff0af264f287db
Demetrius Albuquerque <demetrius.albuquerque@yahoo.com.br>
2018-02-17 20:31:42 -0400

    added token session_controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,21 @@
 class Users::SessionsController < Devise::SessionsController
-  skip_before_action :authenticate_user!
+
+  def create
+    user = warden.authenticate!(auth_options)
+    token = Tiddle.create_and_return_token(user, request)
+    render json: { authentication_token: token }
+  end
+
+  def destroy
+    Tiddle.expire_token(current_user, request) if current_user
+    render json: {}
+  end
+
+  private
+  # this is invoked before destroy and we have to override it
+  def verify_signed_out_user
+  end
+  # skip_before_action :authenticate_user!
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
